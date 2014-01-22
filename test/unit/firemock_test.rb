39568cb29306afec587c9f25faded754a4c36f5b
subrahmanyam <sivagollapalli@yahoo.com>
2014-01-22 22:35:39 +0530

    Using latest minitest class name convention

--- test/unit/firemock_test.rb
+++ test/unit/firemock_test.rb
@@ -3,49 +3,49 @@ require 'minitest/fire_mock'
 
 class FireMockTest < MiniTest::Unit::TestCase
   def test_mock_is_valid_when_not_defined
-    mock = MiniTest::FireMock.new('NotDefinedConstant')
+    mock = Minitest::FireMock.new('NotDefinedConstant')
     mock.expect(:defined_method, 42)
     assert_equal 42, mock.defined_method
     mock.verify
   end
 
   def test_mock_is_valid_when_defined_and_responds_to_method
-    mock = MiniTest::FireMock.new('DefinedConstant')
+    mock = Minitest::FireMock.new('DefinedConstant')
     mock.expect(:defined_method, 42)
     assert_equal 42, mock.defined_method
     mock.verify
   end
 
   def test_mock_is_invalid_when_defined_but_dont_responds_to_method
-    mock = MiniTest::FireMock.new('DefinedConstant')
+    mock = Minitest::FireMock.new('DefinedConstant')
     assert_raises MockExpectationError, "expected Foo to define `not_defined_method`, but it doesn't" do
       mock.expect(:not_defined_method, 42)
     end
   end
 
   def test_mock_with_namespace_is_valid_when_not_defined
-    mock = MiniTest::FireMock.new('Namespace::NotDefinedConstant')
+    mock = Minitest::FireMock.new('Namespace::NotDefinedConstant')
     mock.expect(:defined_method, 42)
     assert_equal 42, mock.defined_method
     mock.verify
   end
 
   def test_mock_with_namespace_is_valid_when_defined_and_responds_to_method
-    mock = MiniTest::FireMock.new('Namespace::NamespacedConstant')
+    mock = Minitest::FireMock.new('Namespace::NamespacedConstant')
     mock.expect(:defined_method, 42)
     assert_equal 42, mock.defined_method
     mock.verify
   end
 
   def test_mock_with_namespace_is_invalid_when_defined_but_dont_responds_to_method
-    mock = MiniTest::FireMock.new('Namespace::NamespacedConstant')
+    mock = Minitest::FireMock.new('Namespace::NamespacedConstant')
     assert_raises MockExpectationError, "expected Foo to define `not_defined_method`, but it doesn't" do
       mock.expect(:not_defined_method, 42)
     end
   end
 
   def test_valid_mock_with_different_arity
-    mock = MiniTest::FireMock.new('DefinedConstant')
+    mock = Minitest::FireMock.new('DefinedConstant')
     assert_raises MockExpectationError, "`defined_method` expects 0 arguments, given 3" do
       mock.expect(:defined_method, 42, [1,2,3])
     end
